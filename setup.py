4bb0834af5c6925415ef01d7567a0f9bce29c6d9
Mark Reynoso <mreynoso@spu.edu>
2017-10-23 20:03:52 -0700

    added size, search, remove, and display functions. working on len and print - not done

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-rom setuptools import setup
+from setuptools import setup
 
 setup(
     name='data-structures',
--- src/linked_list.py
+++ src/linked_list.py
@@ -19,18 +19,71 @@ class Node(object):
 
 
 class LinkedList(object):
-    def __init__(self, head = None)
+    def __init__(self, head = None):
         self.head = head
+        self.size = 0
 
-    def push(val):
+
+    def push(self, val):
         new_node = Node(val)
         new_node.set_next(self.head)
         self.head = new_node
+        self.size += 1
 
 
-    def pop():
+    def pop(self):
         current = self.head
         self.head = current.get_next()
+        self.size -= 1
         return current
 
 
+    def size(self):
+        print(self.size)
+        return self.size
+
+
+    def search(self, val):
+        current = self.head
+        while current:
+            if current.get_data() == val:
+                print('current')
+                return current
+            else:
+                current = current.get_next()
+        return None
+
+
+    def remove_node(self, data):
+        current = self.head
+        prev = None
+        while current:
+            if current.get_data() == data:
+                if current == self.head:
+                    self.head = current.get_next()
+                    self.size -= 1
+                else:
+                    prev.set_next(current.get_next())
+                    self.size -= 1
+                return current
+            prev = current
+            current = current.get_next()
+        else:
+            raise ValueError('Your node does not exist in this linked list.')
+
+
+    def display(self):
+        print_list = []
+        current = self.head
+        while current:
+            print_list.append(current.get_data())
+            current = current.get_next()
+        print(str(tuple(print_list)))
+
+
+    def __str__(self):
+        self.display()
+
+
+    def __len__(self):
+        self.size()
