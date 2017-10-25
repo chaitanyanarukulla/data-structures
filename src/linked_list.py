3a91e5b6d3b1fdd330f224eaf924d14631f5d8ad
Chaitanya.Narukulla <Chai@Ashleys-MacBook-Air.local>
2017-10-25 10:39:45 -0700

    Doc strings Done

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,25 +1,32 @@
-"""DATA"""
+"""DATA Structure for linked list"""
 
 class Node(object):
+    """This create a class of Node"""
     def __init__(self, data = None, next = None):
+    """This function creates instance of class Node"""
         self.data = data
         self.next = next
 
 
     def get_data(self):
+    """This function  sets  self  to data"""
         return self.data
 
 
     def get_next(self):
+    """This function  return next of data"""
         return self.next
 
 
     def set_next(self, new_next):
+     """This function  sets data to next data"""
         self.next = new_next
 
 
 class LinkedList(object):
+    """This create a class of LinkedList"""
     def __init__(self, iterable=()):
+    """This function creates instance of LinkedList"""
         self.head = None
         self._size = 0
         if isinstance(iterable, (str, tuple, list)):
@@ -28,6 +35,7 @@ class LinkedList(object):
 
 
     def push(self, val):
+        """This function creates new node and makes it a head"""
         new_node = Node(val)
         new_node.set_next(self.head)
         self.head = new_node
@@ -35,6 +43,7 @@ class LinkedList(object):
 
 
     def pop(self):
+    """This function delets the first node and mkes next node a head"""
         if self.head is None:
             raise IndexError('This is an empty list. No values to pop.')
         current = self.head
@@ -44,11 +53,13 @@ class LinkedList(object):
 
 
     def size(self):
+    """This function returns the length"""
         print(self._size)
         return self._size
 
 
     def search(self, val):
+    """This function searchs the val"""
         current = self.head
         while current:
             if current.get_data() == val:
@@ -60,6 +71,7 @@ class LinkedList(object):
 
 
     def remove_node(self, data):
+    """This function serches the value and removes a node of that value and links the between nodes"""
         current = self.head
         prev = None
         while current:
@@ -78,6 +90,7 @@ class LinkedList(object):
 
 
     def display(self):
+    """This function displays the data in str of tuples"""
         print_list = []
         current = self.head
         while current:
@@ -88,6 +101,7 @@ class LinkedList(object):
 
 
     def __str__(self):
+    """This function returns the displays of data in str of tuples"""
         return str(self.display())
 
 
--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -44,7 +44,7 @@ def test_linked_list_push_moves_old_head_to_new_next():
 
 
 def test_linked_list_pop_removes_head():
-    """."""
+    """Testing linkedlist for pop function and if it removes the head """
     from linked_list import LinkedList
     l = LinkedList()
     l.push('val')
@@ -53,7 +53,7 @@ def test_linked_list_pop_removes_head():
 
 
 def test_linked_list_pop_returns_removed_head():
-    """."""
+    """Testing linkedlist for pop function if it returns proper head """
     from linked_list import LinkedList
     l = LinkedList()
     l.push('val')
@@ -62,7 +62,7 @@ def test_linked_list_pop_returns_removed_head():
 
 
 def test_linked_list_pop_with_multiple_items_in_list():
-    """."""
+   """Testing linkedlist for pop with multiple items in list """
     from linked_list import LinkedList
     l = LinkedList()
     l.push('val')
@@ -72,7 +72,7 @@ def test_linked_list_pop_with_multiple_items_in_list():
 
 
 def test_linked_list_pop_on_empty_list_returns_exception():
-    """."""
+     """Testing linkedlist for pop on empty list if it returns exception """
     from linked_list import LinkedList
     l = LinkedList()
     with pytest.raises(IndexError):
@@ -80,7 +80,7 @@ def test_linked_list_pop_on_empty_list_returns_exception():
 
 
 def test_linked_list_size_returns_none_on_empty_list():
-    """."""
+     """Testing linkedlist for te size returns none on empty list """
     from linked_list import LinkedList
     l = LinkedList()
     assert l.size() == 0
@@ -88,7 +88,7 @@ def test_linked_list_size_returns_none_on_empty_list():
 
 @pytest.mark.parametrize('n', range(20))
 def test_linked_list_size_returns_list_length(n):
-    """Linked List should calculate its own size."""
+    """Testing Linked List should calculate its own size."""
     from linked_list import LinkedList
     l = LinkedList()
     for i in range(n):
@@ -98,7 +98,7 @@ def test_linked_list_size_returns_list_length(n):
 
 @pytest.mark.parametrize('n', range(20))
 def test_linked_list_len_uses_length_function(n):
-    """Linked List should successfully interact with the len() in Python."""
+    """Testing Linked List should successfully interact with the len() in Python."""
     from linked_list import LinkedList
     l = LinkedList()
     for i in range(n):
@@ -107,14 +107,14 @@ def test_linked_list_len_uses_length_function(n):
 
 
 def test_linked_list_search_empty_returns_none():
-    """."""
+    """Tsting Linked List  for search that empty data returns none"""
     from linked_list import LinkedList
     l = LinkedList()
     assert l.search(0) is None
 
 
 def test_linked_list_search_one_node_list_returns_head():
-    """."""
+    """ Testing Linked List if it serches node and returns head"""
     from linked_list import LinkedList
     l = LinkedList()
     l.push('val')
@@ -123,7 +123,7 @@ def test_linked_list_search_one_node_list_returns_head():
 
 @pytest.mark.parametrize('n', range(1, 10))
 def test_linked_list_search_returns_input_value(n):
-    """."""
+    """ Testing Linked List for search returns input value"""
     from linked_list import LinkedList
     from random import randint
     l = LinkedList()
@@ -134,7 +134,7 @@ def test_linked_list_search_returns_input_value(n):
 
 
 def test_linked_list_accepts_iterables():
-    """."""
+   """ Testing Linked List to accepts for iterables"""
     from linked_list import LinkedList
     the_list = [1, 2, 3, 4, 5]
     l = LinkedList(the_list)
@@ -143,7 +143,7 @@ def test_linked_list_accepts_iterables():
 
 
 def test_linked_list_accepts_each_iterable_value():
-    """."""
+   """ Testing Linked List if accepts each iterable value"""
     from linked_list import LinkedList
     the_list = [1, 2, 3, 4, 5]
     l = LinkedList(the_list)
@@ -152,7 +152,7 @@ def test_linked_list_accepts_each_iterable_value():
 
 
 def test_linked_list_remove_node_removes_input_value():
-    """."""
+   """ Testing Linked List to remove node by value"""
     from linked_list import LinkedList
     the_list = [1, 2, 3, 4, 5]
     l = LinkedList(the_list)
@@ -160,7 +160,7 @@ def test_linked_list_remove_node_removes_input_value():
 
 
 def test_linked_list_remove_node_removes_sets_next_as_head():
-    """."""
+    """ Testing Linked List to remove node removes sets next as head"""
     from linked_list import LinkedList
     the_list = [1, 2, 3, 4, 5]
     l = LinkedList(the_list)
@@ -169,7 +169,7 @@ def test_linked_list_remove_node_removes_sets_next_as_head():
 
 
 def test_linked_list_remove_node_raises_ValueError_if_input_not_exist():
-    """."""
+    """ Testing Linked List to remove node raises ValueError if input not exist"""
     from linked_list import LinkedList
     the_list = [1, 2, 3, 4, 5]
     l = LinkedList(the_list)
@@ -178,7 +178,7 @@ def test_linked_list_remove_node_raises_ValueError_if_input_not_exist():
 
 
 def test_linked_list_display_returns_tuple_of_list_values():
-    """."""
+    """testing for test linked list display returns tuple of list values"""
     from linked_list import LinkedList
     the_list = [1, 2, 3, 4, 5]
     l = LinkedList(the_list)
@@ -186,7 +186,7 @@ def test_linked_list_display_returns_tuple_of_list_values():
 
 
 def test_linked_list_len_returns_list_length():
-    """."""
+    """Testing linked list for len returns list length"""
     from linked_list import LinkedList
     the_list = [1, 2, 3, 4, 5]
     l = LinkedList(the_list)
@@ -194,7 +194,7 @@ def test_linked_list_len_returns_list_length():
 
 
 def test_linked_list_str_calls_display():
-    """."""
+    """Testing for str calls display"""
     from linked_list import LinkedList
     the_list = [1, 2, 3, 4, 5]
     l = LinkedList(the_list)
