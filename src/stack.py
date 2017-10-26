91298368357378bd384fa24448814169ca6e43d2
Mark Reynoso <mreynoso@spu.edu>
2017-10-25 17:01:42 -0700

    all tests pass!

--- src/stack.py
+++ src/stack.py
@@ -1,11 +1,12 @@
-"""."""
+"""Create a Stack class composed from instance of LinkedList()."""
 from linked_list import LinkedList
 
+
 class Stack(object):
+    """Simple class from LinkdedList() instance."""
+
     def __init__ (self, iterable=()):
-        self.func = LinkedList(iterable=())
-        self.head = self.func.head
-        self.length = self.func._size
+        self.func = LinkedList(iterable)
 
 
     def push(self, val):
@@ -13,9 +14,8 @@ class Stack(object):
 
 
     def pop(self):
-        return self.func.pop()
+        return self.func.pop().data
 
 
     def __len__(self):
         return self.func._size
-s
