7e1545a0e7a7e75c9e838e1d89f31203156d7258
Mark Reynoso <mreynoso@spu.edu>
2017-10-25 21:20:53 -0700

    fixed a linter error

--- src/stack.py
+++ src/stack.py
@@ -5,17 +5,14 @@ from linked_list import LinkedList
 class Stack(object):
     """Simple class from LinkdedList() instance."""
 
-    def __init__ (self, iterable=()):
+    def __init__(self, iterable=()):
         self.func = LinkedList(iterable)
-
 
     def push(self, val):
         return self.func.push(val)
 
-
     def pop(self):
         return self.func.pop().data
-
 
     def __len__(self):
         return self.func._size
