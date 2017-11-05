ffd41ab12aadd341e0e6007d217860acb6e3f6b3
Chaitanya.Narukulla <Chai@Ashleys-MacBook-Air.local>
2017-11-05 12:32:48 -0800

    update readme

---
+++ src/priorityq.py
@@ -0,0 +1,42 @@
+"""Create a priority queue instance."""
+
+
+class Priorityq(object):
+    """Create a que with priority attributes for each value."""
+
+    def __init__(self):
+        """Initialize a priority queue instance."""
+        self._que = {}
+        self._highest = 0
+        self._lowest = 0
+
+    def insert(self, val, priority=0):
+        """Insert a new value into the queue."""
+        if priority <= self._highest:
+            self._highest = priority
+        if priority >= self._lowest:
+            self._lowest = priority
+        if priority in self._que:
+            self._que[priority].append(val)
+        else:
+            self._que[priority] = [val]
+
+    def pop(self):
+        """Remove the highest prioty value from queue."""
+        if len(self._que) == 0:
+            raise IndexError('There are no value to pop.')
+        else:
+            val = self._que[self._highest].pop(0)
+            if self._que[self._highest] == []:
+                self._que.pop(self._highest)
+                if len(self._que) == 0:
+                    self._highest = 0
+                    self._lowest = 0
+                else:
+                    high = min(self._que)
+                    self._highest = high
+            return val
+
+    def peek(self):
+        """View the hightest priority item."""
+        return self._que[self._highest][0]
---
+++ src/test_priorityq.py
@@ -0,0 +1,122 @@
+"""Test priorityq.py."""
+import pytest
+
+
+def test_priorityq_iinitialize_empty_que(pq):
+    """Test initialize an empty pq."""
+    assert pq
+
+
+def test_priorityq_insert_one_val_w_priority(pq):
+    """Test insert one value to pq with priority."""
+    pq.insert('hi', 0)
+    assert pq._highest == 0
+
+
+def test_insert_n_values_returns_n_length(pq):
+    """Test that the number of values inserted is the que's length."""
+    for i in range(20):
+        pq.insert(i)
+    total_length = 0
+    for key in pq._que:
+        total_length += len(pq._que[key])
+    assert total_length == 20
+
+
+def test_priorityq_insert_with_many_priority_returns_higest(pq):
+    """Test ipriorityq insert with many priority returns higest pirority."""
+    pq.insert('hi', -10)
+    pq.insert('one', 1)
+    pq.insert('four', 4)
+    pq.insert('eight', 8)
+    assert pq._highest == -10
+
+
+def test_priorityq_insert_with_many_priority_returns_lowest(pq):
+    """Test ipriorityq insert with many priority returns lowest pirority."""
+    pq.insert('hi', -10)
+    pq.insert('one', 1)
+    pq.insert('four', 4)
+    pq.insert('eight', 8)
+    assert pq._lowest == 8
+
+
+def test_priorityq_insert_with_same_priority_returns_list_of_val(pq):
+    """Test if inserted with same priority returns list of val in pirority."""
+    pq.insert('hi', -10)
+    pq.insert('one', -10)
+    pq.insert('four', 4)
+    pq.insert('eight', 4)
+    assert pq._que[-10] == ['hi', 'one']
+
+
+def test_if_pop_raises_indexerror_on_empty_prorityq(pq):
+    """Test if pop reises indexerroe."""
+    with pytest.raises(IndexError):
+        pq.pop()
+
+
+def test_priorityq_pop_function_pops(pq):
+    """Test ipriorityq if it pops a val."""
+    pq.insert('hi', -10)
+    pq.insert('one', 1)
+    pq.pop()
+    pq.pop()
+    assert pq._que == {}
+
+
+def test_priorityq_pop_function_returns_val(pq):
+    """Test ipriorityq if it pops and returns the right val."""
+    pq.insert('hi', -10)
+    pq.insert('one', 1)
+    assert pq.pop() == 'hi'
+
+
+def test_pop_if_more_poped_then_inserted_raises_error(pq):
+    """Test if inserted with same priority returns list of val in pirority."""
+    pq.insert('hi', -10)
+    pq.insert('one', -10)
+    pq.insert('four', 4)
+    pq.pop()
+    pq.pop()
+    pq.pop()
+    with pytest.raises(IndexError):
+        pq.pop()
+
+
+def test_pop_if_returns_in_prioety_order(pq):
+    """Test if inserted with priority returns in priority order."""
+    pq.insert('hi', -10)
+    pq.insert('one', -1)
+    pq.insert('four', 4)
+    pop1 = pq.pop()
+    pop2 = pq.pop()
+    pop3 = pq.pop()
+    assert pop1 == 'hi'
+    assert pop2 == 'one'
+    assert pop3 == 'four'
+
+
+def test_pop_if_returns_in_prioety_order_pop_in_and_out(pq):
+    """Test if inserted val with priority returns in priority order."""
+    pq.insert('hi', -10)
+    pq.insert('four', 4)
+    pq.insert('one', -1)
+    pop1 = pq.pop()
+    pop2 = pq.pop()
+    pq.insert('first', -20)
+    pop3 = pq.pop()
+    pq.insert('last', 20)
+    pop4 = pq.pop()
+    assert pop1 == 'hi'
+    assert pop2 == 'one'
+    assert pop3 == 'first'
+    assert pop4 == 'four'
+
+
+def test_peek_returns_the_higest_priority_val(pq):
+    """Test if peek returns the val in line to be poped."""
+    pq.insert('hi', -10)
+    pq.insert('one', 1)
+    pq.insert('four', 4)
+    assert pq.peek() == 'hi'
