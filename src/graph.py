a73a4b173be04d9ea8a2673b2b215fe3447c3176
Mark Reynoso <mreynoso@spu.edu>
2017-11-08 14:36:16 -0800

    converted graph to dictionaries and added weight to edges

--- src/graph.py
+++ src/graph.py
@@ -14,30 +14,30 @@ class Graph(object):
 
     def edges(self):
         """Return a list of edges in graph."""
-        edges = []
+        edges = {}
         for key in self._graph:
             for i in self._graph[key]:
-                edges.append((key, i))
+                edges[(key, i)] = self._graph[key][i]
         return edges
 
     def add_node(self, val):
         """Add a node with value of val to graph."""
-        self._graph[val] = []
+        self._graph[val] = {}
 
-    def add_edge(self, val1, val2):
+    def add_edge(self, val1, val2, weight=0):
         """Add a new edge to graph between val1 & val2 as well as add vals."""
         if val1 in self._graph and val2 in self._graph:
             if val2 not in self._graph[val1]:
-                self._graph[val1].append(val2)
+                self._graph[val1][val2] = weight
         elif val1 in self._graph and val2 not in self._graph:
-            self._graph[val2] = []
-            self._graph[val1].append(val2)
+            self._graph[val2] = {}
+            self._graph[val1][val2] = weight
         elif val2 in self._graph and val1 not in self._graph:
-            self._graph[val1] = []
-            self._graph[val1].append(val2)
+            self._graph[val1] = {}
+            self._graph[val1][val2] = weight
         else:
-            self._graph[val1] = [val2]
-            self._graph[val2] = []
+            self._graph[val1] = {val2: weight}
+            self._graph[val2] = {}
 
     def del_node(self, val):
         """Delete node w/val from graph, raises exception if not exist."""
@@ -46,7 +46,7 @@ class Graph(object):
             for key in self._graph:
                 for i in self._graph[key]:
                     if i == val:
-                        self._graph[key].remove(val)
+                        del self._graph[key]
         else:
             raise ValueError('There is no node of that value in the graph.')
 
@@ -54,11 +54,12 @@ class Graph(object):
         """Delete edge between val1 & val2 from graph."""
         try:
             if val2 in self._graph[val1]:
-                self._graph[val1].remove(val2)
+                del self._graph[val1]
             else:
                 raise ValueError('These edges do not exist.')
         except KeyError:
             raise ValueError('These edges do not exist.')
+
 
     def has_node(self, val):
         """Return true or false if node has value."""
@@ -70,7 +71,7 @@ class Graph(object):
     def neighbors(self, val):
         """Return list of nodes connected node(val)."""
         try:
-            neighbors = self._graph[val]
+            neighbors = []
             for key in self._graph:
                 if val in self._graph[key]:
                     if self._graph[key] not in neighbors:
@@ -99,7 +100,7 @@ class Graph(object):
                 val = path.pop()
                 if val not in depth_traversal:
                     depth_traversal.append(val)
-                    path = path + self._graph[val]
+                    path = path + list(self._graph[val].keys())
             return depth_traversal
         else:
             raise ValueError('Value is not in graph.')
@@ -113,7 +114,7 @@ class Graph(object):
                 val = path.pop(0)
                 if val not in depth_traversal:
                     depth_traversal.append(val)
-                    path = path + self._graph[val]
+                    path = path + list(self._graph[val].keys())
             return depth_traversal
         else:
             raise ValueError('Value is not in graph.')
