139d32f7c5d12de49bbc39cfb61e8292e34fa879
Mark Reynoso <mreynoso@spu.edu>
2017-12-03 12:29:02 -0800

    horner hash added, tests complete and passing

--- README.md
+++ README.md
@@ -272,3 +272,22 @@ new.post_order()
 new.breadth_first()
 new.delete(val)
 ```
+
+### Hash Table
+
+```
+Using a naive hash and slightly more complex hash with Horner's Rule, HashTable() will store based on the supplied key, which must be a string. By default, HashTable() uses the naive hash method, but can be changed on initialization. 
+
+To create an instance if the hash table, HashTable(), from python:
+
+new = HashTable() *you may initiate the table with any size, must be integer, as the first arguement in the function. You may also pass in an alternative hashing method. A naive hash will be used my defaul, but you may also choose `horner_hash` as the second arguement on initialization, or any hashing function you choose to import. For example: HashTable(100000, horner_hash).*
+
+HashTable() contains the following methods:
+* _set(key, val) (O(n))_ - sets a value at a given key (must me string) in the table, if key already has value it will be set to new value.
+* _get(key) (0(n))_ - retrieve the value of the item with the given key.
+* _ _hash(key) (O(n))_ - hashes key and returns an integer between 0 and the size of the table.
+
+To access any contained methods:
+new.set(key, val)
+new.get(key)
+```
--- src/hash_table.py
+++ src/hash_table.py
@@ -1,23 +1,27 @@
 """Hash table for storing strings."""
 
 
-def simple_hash(data, buckets):
+def naive_hash(word, buckets):
     """A simple hash for strings."""
     hash_val = 0
-    for letter in data:
+    for letter in word:
         hash_val += ord(letter)
     return hash_val % buckets
 
 
-def complex_hash(data):
-    """A hash for strings."""
-    pass
+def horner_hash(word, buckets):
+    """Using horner's rule for hashing key."""
+    constant = 37
+    result = 0
+    for letter in word:
+        result = result * constant + ord(letter)
+    return result % buckets
 
 
 class HashTable(object):
     """Hash table class for hashing strings."""
 
-    def __init__(self, size=10, hash_func=simple_hash):
+    def __init__(self, size=10, hash_func=naive_hash):
         """Initialize a new hashtable."""
         self.hash_func = hash_func
         self._size = size
@@ -29,9 +33,9 @@ class HashTable(object):
         if self._buckets[hash_key] == []:
             return 'There are no items with that key.'
         else:
-            for i in range(len(self._buckets[hash_key])):
-                if self._buckets[hash_key][i][0] == key:
-                    return self._buckets[hash_key][i][1]
+            for idx, item in enumerate(self._buckets[hash_key]):
+                if item[0] == key:
+                    return self._buckets[hash_key][idx][1]
                 else:
                     return 'There are not items with that key.'
 
@@ -43,10 +47,10 @@ class HashTable(object):
         if self._buckets[hash_key] == []:
             self._buckets[hash_key].append((key, val))
         else:
-            for i in range(len(self._buckets[hash_key])):
-                if self._buckets[hash_key][i][0] == key:
-                    gone = self._buckets[hash_key][i][1]
-                    self._buckets[hash_key][i] = (key, val)
+            for idx, item in enumerate(self._buckets[hash_key]):
+                if item[0] == key:
+                    gone = item[1]
+                    self._buckets[hash_key][idx] = (key, val)
                     return 'Your data {} has been updated to {} at key {}.'\
                         .format(gone, val, key)
                 else:
